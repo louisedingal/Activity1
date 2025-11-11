31b1942894509a08ee4b2ed788bac403cdb5840c
louisedingal <luckydingal46@gmail.com>
2025-11-11 22:58:42 +0800

    Add ActiVity2 files

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/security', name: 'app_security')]
+    public function index(): Response
+    {
+        return new Response('<h1>Welcome to the Dashboard!</h1>');
+    }
+}
